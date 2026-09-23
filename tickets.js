159fc10f4d7e728eae9c7803faf0672aed0e99eb
kimberlykatepelone18-ui <kimberlykatepelone18@gmail.com>
2026-09-23 18:52:18 +0800

    Add flat  discount to ticket orders - pelone.kimberlykate
    :wq#

--- tickets.js
+++ tickets.js
@@ -1,19 +1,21 @@
 function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
- 
-function calculateTicketPrice(quantity, basePrice, isPremium = false) {
-  let total = quantity * basePrice;
+
+function calculateTicketPrice(quantity, basePrice, seatingType) {
+  let price = quantity * basePrice;
 
   if (quantity >= 5) {
-    total *= 0.90;
+    price *= 0.90;
   }
 
-  if (isPremium) {
-    total *= 1.50;
+  if (seatingType === "premium") {
+    price *= 1.50;
   }
 
-  return Math.round(total);
+  price = price - 10;
+
+  return Math.round(price);
 }
- 
+
 module.exports = { isValidQuantity, calculateTicketPrice };
